5927f876699c31d03cf22fb31fbc41903f7c90ac
Diego Souza <souza.diegoty@gmail.com>
2024-07-24 13:30:19 -0300

    Fix - Ajuste no beforeEach do teste Pedido.cy

--- cypress/e2e/pedido.cy.js
+++ cypress/e2e/pedido.cy.js
@@ -3,12 +3,12 @@ const Pedido = require('../pageObjects/pedido')
 beforeEach(() => {
     cy.visit('');
     cy.login();
+    cy.adicionarItemCarrinho();
+    cy.realizarPedido();
 })
 
 describe('[Fluxo Feliz] - Tela de Pedidos', () => {
     it('Consultar detalhes do pedido após realizar uma compra.', () => {
-        cy.adicionarItemCarrinho();
-        cy.realizarPedido();
         Pedido.ClicarNumeroPedido();
     })
 })
